9c38922473b45d1c3a0cc687a41cc965777f5bbc
herferjos <120600448+herferjos@users.noreply.github.com>
2023-11-23 17:18:05 +0100

    udpate

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ if 'planning' in st.session_state:
     st.write("## Routes")
     for i in range(len(st.session_state.planning['order'])):
         day = st.session_state.planning['order'][i]
-        st.write(st.session_state.planning['order'])
 
         for j in range(len(day) - 1):
             st.markdown(f"_**From {day[j]} To {day[j+1]}**_")
