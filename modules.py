f94603e96e585078650c661eeadf30628fa7275f
herferjos <120600448+herferjos@users.noreply.github.com>
2023-11-23 17:49:49 +0100

    s

--- modules.py
+++ modules.py
@@ -12,7 +12,7 @@ client2 = openrouteservice.Client(key=st.secrets['route_key'])
 search = DuckDuckGoSearchRun()
 
 @st.cache_data(persist="disk")
-def get_map(cord_long_1, cord_lat_1, cord_long_2, cord_lat_2):
+def get_map(day, cord_long_1, cord_lat_1, cord_long_2, cord_lat_2):
 
     m = folium.Map(location=[cord_lat_1,cord_long_1], zoom_start = 16)
 
